97c27ff1c9621ff3f89d5636b7258261fab7a00f
Alex <aleksejtatukov098@gmail.com>
2023-12-19 20:04:22 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -7,43 +7,43 @@
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
 int count = 0;
-int maxElement = 0;
+int max2 = 0;
 int max = 0;
 //int maxElemen2 = 0
-int [,] CreateArray(int columns, int rows)
+int[,] CreateArray(int columns, int rows)
 {
-    int [,] Array = new int [columns, rows];
+    int[,] Array = new int[columns, rows];
 
     for (int i = 0; i < Array.GetLength(0); i++)
     {
-       
+
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-          Array[i, j] = new Random(). Next(0, 10);
-          for (int k = 0; k < Array.GetLength(1) - 1; k++)
-          {
-            if (count == rows)
-            {
-                if (max > maxElement) maxElement = max;
-                max = 0;
-            } 
-             max += Array[i, k];
-            count++;
-          }
+            Array[i, j] = new Random().Next(0, 10);
+        max += Array[i, j];
+        
+
+        for (int k = 0; k < Array.GetLength(0) - 1; k++)
+        {
+            for (int l = 0;  l < Array.GetLength(1); l++)
+           
+            max2 += Array[k, l];
         }
-    Console.WriteLine(maxElement);
+        }
+        Console.WriteLine();
+        if (max2 > max) max = max2;
     }
     return Array;
 }
-void PrintArray(int [,] Array)
+void PrintArray(int[,] Array)
 {
-     for (int i = 0; i < Array.GetLength(0); i++)
+    for (int i = 0; i < Array.GetLength(0); i++)
     {
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-         Console.Write(Array[i, j] + "\t");  
+            Console.Write(Array[i, j] + "\t");
         }
-Console.WriteLine();
+        Console.WriteLine();
     }
 }
 PrintArray(CreateArray(5, 5));
